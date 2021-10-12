9a9f9d6bfce092cf9a509a935ca50b49e6b4b6a4
mohammad_saadati <saadati.m.b@gmail.com>
2021-10-13 00:48:54 +0330

    use grid components of vuetify

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,10 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
       <chart :data="barChartData" :options="barChartOptions" :height="200" />
+    </v-col>
+  </v-row>
+</template>
 import _ from 'lodash';    
 import Chart from '~/components/Chart'
   async fetch() {
